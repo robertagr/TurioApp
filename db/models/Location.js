4cbdfffe6ddbc20d50d73c838b5b79d317b7e7e2
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-07-14 11:53:42 +0200

    update and delete done

--- db/models/Location.js
+++ db/models/Location.js
@@ -5,25 +5,24 @@ const { Schema } = mongoose;
 const locationSchema = new Schema({
   name: {
     type: String,
-    required: true,
+    // required: true,
   },
   location: {
     type: String,
-    required: true,
+    // required: true,
   },
   image: {
     type: String,
-    required: true,
+    // required: true,
   },
   mapURL: {
     type: String,
-    required: true,
+    // required: true,
   },
   description: {
     type: String,
-    required: true,
+    // required: true,
   },
-//   reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
 });
 
 const Location =
